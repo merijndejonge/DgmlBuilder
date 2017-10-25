f0a5d4ed57a68c60307f1c5f52cad175d77d3e17
Merijn de Jonge <merijn.de.jonge@gmail.com>
2017-10-25 13:59:01 +0200

    Bug fix: Catery refs were not serialized

--- src/DgmlBuilder/Model/SerializerHelper.cs
+++ src/DgmlBuilder/Model/SerializerHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -6,25 +7,52 @@ namespace OpenSoftware.DgmlTools.Model
 {
     public static class SerializerHelper
     {
-        internal static void ToXml<T>(this T obj, XmlWriter writer) where T : ICustomPropertiesProvider
+        private static void ToXmlGeneric(object obj, XmlWriter writer)
         {
-            var properties = typeof(T).GetProperties();
-            var serializeableProperties =
+            var properties = obj.GetType().GetProperties();
+            var xmlAttributes =
                 properties.Where(x => x.GetCustomAttributes(typeof(XmlAttributeAttribute), true).Any());
 
-            foreach (var serializeableProperty in serializeableProperties)
+            foreach (var xmlAttribute in xmlAttributes)
             {
-                var value = serializeableProperty.GetValue(obj);
-                if (serializeableProperty.PropertyType.IsValueType || value != null)
+                var value = xmlAttribute.GetValue(obj);
+                if (xmlAttribute.PropertyType.IsValueType || value != null)
                 {
-                    writer.WriteAttributeString(serializeableProperty.Name, value.ToString());
+                    writer.WriteAttributeString(xmlAttribute.Name, value.ToString());
                 }
             }
+            var xmlElements = properties.Where(x => x.GetCustomAttributes(typeof(XmlElementAttribute), true).Any());
+            foreach (var xmlElement in xmlElements)
+            {
+                var value = xmlElement.GetValue(obj);
+                if (!xmlElement.PropertyType.IsValueType && value == null) continue;
+                var attribute = (XmlElementAttribute)xmlElement.GetCustomAttributes(typeof(XmlElementAttribute), true).Single();
+                if (value is IEnumerable list)
+                {
+                    foreach (var element in list)
+                    {
+                        var elementName = attribute.ElementName ?? element.GetType().Name;
+                        writer.WriteStartElement(elementName);
+                        ToXmlGeneric(element, writer);
+                        writer.WriteEndElement();
+                    }
+                }
+                else
+                {
+                    var elementName = attribute.ElementName ?? xmlElement.Name;
+                    writer.WriteElementString(elementName, value.ToString());
+                }
+            }
+        }
+        internal static void ToXml<T>(this T obj, XmlWriter writer) where T : ICustomPropertiesProvider
+        {
+            ToXmlGeneric(obj, writer);
 
             foreach (var dataElement in obj.Properties)
             {
                 writer.WriteAttributeString(dataElement.Key, dataElement.Value.ToString());
             }
+
         }
     }
 }
