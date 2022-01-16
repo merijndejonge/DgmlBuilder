be000eb884d0caee0ae1996b5940f003af545f92
Greg Ingram <greg.ingram@carrier.com>
2022-01-16 09:21:05 -0500

    Remove unused usings

--- src/DgmlBuilder/DgmlBuilder.cs
+++ src/DgmlBuilder/DgmlBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using OpenSoftware.DgmlTools.Builders;
 using OpenSoftware.DgmlTools.Model;
--- src/DgmlBuilder/Model/Layout.cs
+++ src/DgmlBuilder/Model/Layout.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenSoftware.DgmlTools.Model;
+﻿namespace OpenSoftware.DgmlTools.Model;
 
 public enum Layout
 {
