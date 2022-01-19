18aff68a119b1a3a722811086a93642a4a2778f6
Greg Ingram <greg.ingram@carrier.com>
2022-01-19 17:31:58 -0500

    Move IGraphAnalysis to related folder and namespace

--- src/DgmlBuilder/GlobalUsings.cs
+++ src/DgmlBuilder/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using OpenSoftware.DgmlTools.Builders;
+﻿global using OpenSoftware.DgmlTools.Analyses;
+global using OpenSoftware.DgmlTools.Builders;
 global using OpenSoftware.DgmlTools.Comparers;
 global using OpenSoftware.DgmlTools.Model;
 global using System;
